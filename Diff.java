3462ea8a1b2d1337560252b41f00bd913c728969
Swatilekha Mitra <swatilekhamitra4@gmail.com>
2022-01-11 03:34:21 +0530

    added Diff.java

--- Diff.java
+++ Diff.java
@@ -3,6 +3,6 @@ public class Diff{
     public static void main(String[] args){
         int a = 14;
         int b = 7;
-        System.out.println(a/b);
+        System.out.println(" "(a/b));
     }
 }
